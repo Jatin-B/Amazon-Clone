5ec3a7f691e4cefdf11ec85e3a0df614aff1d571
Jatin-B <jbaweja24@gmail.com>
2024-09-12 00:55:45 +0530

    Version-4

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -104,5 +104,5 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     removeFromCart(productId);
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
     container.remove();
-  })
+  });
 });
